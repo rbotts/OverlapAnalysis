d23877fa3e2a9f61f2427a6e0e79d08dcfbb2c92
TJ Wiegman <tjwieg@users.noreply.github.com>
2017-05-17 16:50:18 -0700

    Added overlap estimation and bootstrap samples to example.R +smoothed bootstrap sampling +Overlap estimation of both original sample and bootstrap samples +Plots overlap of two density plots *Still have to calculate confidence interval

--- example1.R
+++ example1.R
@@ -1,9 +1,23 @@
 #Comparing activity patterns of Puma concolor (Puma) and Tayassu tajuca (Collared Peccary)
+require(overlap)
 puma <- subset(ind.data$TimeRad, ind.data["Species"] == "Puma concolor")
 peccary <- subset(ind.data$TimeRad, ind.data["Species"] == "Tayassu tajuca")
+overlapPlot(puma, peccary, lty=c(1,2))
+legend("topleft", c("Puma", "Peccary"), lty=c(1,2), col=c("black", "blue"))
 
+#Drawing smoothed bootstrap samples from each distribution and comparing overlap
+puma.boot <- resample(puma, 10000)
+print("Puma bootstrap successful.")
+peccary.boot <- resample(peccary, 10000)
+print("Peccary bootstrap successful.")
+
+print("Estimating overlap...")
 if (min(length(puma), length(peccary)) <= 75) {
-  print("delta1")
+  puma.peccary.ovl.boot <- overlapEst(puma.boot, peccary.boot, adjust=c(0.8, NA, NA))[1]
+  puma.peccary.ovl <- overlapEst(puma, peccary, adjust=c(0.8, NA, NA))[1]
 } else {
-  print("delta5")
+  puma.peccary.ovl.boot <- overlapEst(puma.boot, peccary.boot, adjust=c(NA, 1, NA))[2]
+  puma.peccary.ovl <- overlapEst(puma, peccary, adjust=c(NA, 1, NA))[2]
 }
+
+print(puma.peccary.ovl.boot)
