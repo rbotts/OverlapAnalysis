c34a19836d2bc61a3d2b0440810880d1fb5053e8
TJ Wiegman <tjwieg@users.noreply.github.com>
2017-06-02 16:53:15 -0700

    Added data grouping for full data set +Grouped nearby/similar sites together for larger sample sizes *Fix nested for loop in overlap.sitepairs.R, currently overshoots index by 1 *Check if previous bug is present in overlap.site.R *Add season column to ind.data

--- R scripts/setup.R
+++ R scripts/setup.R
@@ -16,6 +16,12 @@ if (max.time > 12 & max.time <= 24) {
 } else if (max.time <= 1) {
   ind.data["TimeRad"] <- ind.data["Time"][[1]]*2*pi
 } else print("Unknown time format.")
-ind.data["Site"] <- gsub("\\s*20\\d\\d\\s*|\\s*Spring\\s*|\\s*Summer\\s*|\\s*Fall\\s*", "", ind.data$Survey.Name)
+ind.data["Site"] <- gsub("\\s*20\\d\\d\\s*|\\s*Spring\\s*|\\s*Summer\\s*|\\s*Fall\\s*|\\s*El\\s*|\\s*La\\s*|\\s*National Park\\s*", "", ind.data$Survey.Name)
+
+#Grouping for our full data set; not necessary for sampledata.csv
+ind.data["Site"] <- gsub("\\s*York Univ\\s*", "ASBC", ind.data$Site)
+ind.data["Site"] <- gsub("\\s*Copal\\s*|\\s*Marta\\s*", "Pejibaye", ind.data$Site)
+ind.data["Site"] <- gsub("\\s*PN Carara\\s*|\\s*Carara\\s*", "PNC", ind.data$Site)
+ind.data["Site"] <- gsub("\\s*Tapanti\\s*|\\s*Via Mills\\s*|\\s*Villa Mills\\s*", "PNT", ind.data$Site)
 
 print(table(ind.data$Species))
