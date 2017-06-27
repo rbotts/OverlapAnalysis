9cf301acd1d8a28d5aa3beac96f0dd3600afe57d
TJ Wiegman <tjwieg@users.noreply.github.com>
2017-06-27 15:39:10 -0700

    Initial lunar exploratory study +Initial explore.lunar.R +Added "Lunar" variable to ind.data -Switched to less granular site grouping

---
+++ R scripts/explore.lunar.R
@@ -0,0 +1,18 @@
+#Exploratory study on the effects of lunar cycle on mammal behavior
+require(overlap)
+require(suncalc)
+
+ind.data["Lunar"] <- (getMoonIllumination(ind.data$Date, "phase")[,2])*2*pi
+namelist <- names(table(ind.data$Species))
+
+for (i in 1:length(namelist)) {
+  if (length(ind.data$Lunar[ind.data$Species == namelist[i]]) >= 15) {
+    active <- ind.data$Lunar[ind.data$Species == namelist[i]]
+    fn <- gsub(pattern=" ", replacement="-", x=paste0("~/Downloads/Lunar/",namelist[i],"-lunar.jpg"), fixed = TRUE)
+    jpeg(filename=fn, width=1280, height=720)
+    densityPlot(active, xscale=NA, xlab="Lunar Phase", xaxt="n", main=paste0(namelist[i], ", n = ", length(active)))
+    rug(active, side = 1)
+    axis(side = 1, at = c(0, pi/2, pi, 3*pi/2, 2*pi), labels = c("New Moon", "First Quarter", "Full Moon", "Last Quarter", "New Moon"))
+    dev.off()
+  }
+}
--- R scripts/setup.R
+++ R scripts/setup.R
@@ -19,11 +19,11 @@ if (max.time > 12 & max.time <= 24) {
 ind.data["Site"] <- gsub("\\s*20\\d\\d\\s*|\\s*Spring\\s*|\\s*Summer\\s*|\\s*Fall\\s*|\\s*El\\s*|\\s*La\\s*|\\s*National Park\\s*", "", ind.data$Survey.Name)
 ind.data["Season"] <- ifelse(grepl("Spring", ind.data$Survey.Name), "Spring", ifelse(grepl("Summer", ind.data$Survey.Name), "Summer", ifelse(grepl("Fall", ind.data$Survey.Name), "Fall", "Other")))
 
-ind.data["Coyote"] <- ifelse(grepl("\\s*Bosque de Agua\\s*|\\s*Campanario\\s*|\\s*Copal\\s*|\\s*PNLC\\s*|\\s*PN Carara\\s*|\\s*Carara\\s*|\\s*PNC\\s*", ind.data$Site), TRUE, FALSE) #Only for exploratory coyote study, not necessary for sampledata.csv
+#ind.data["Coyote"] <- ifelse(grepl("\\s*Bosque de Agua\\s*|\\s*Campanario\\s*|\\s*Copal\\s*|\\s*PNLC\\s*|\\s*PN Carara\\s*|\\s*Carara\\s*|\\s*PNC\\s*", ind.data$Site), TRUE, FALSE) #Only for exploratory coyote study, not necessary for sampledata.csv
 
 #Grouping for our full data set; not necessary for sampledata.csv
 ind.data["Site"] <- gsub("\\s*York Univ\\s*", "ASBC", ind.data$Site)
-#ind.data["Site"] <- gsub("\\s*Copal\\s*|\\s*Marta\\s*", "Pejibaye", ind.data$Site) #Comment out during exploratory coyote study
+ind.data["Site"] <- gsub("\\s*Copal\\s*|\\s*Marta\\s*", "Pejibaye", ind.data$Site) #Comment out during exploratory coyote study
 ind.data["Site"] <- gsub("\\s*PN Carara\\s*|\\s*Carara\\s*", "PNC", ind.data$Site)
 ind.data["Site"] <- gsub("\\s*Tapanti\\s*|\\s*Via Mills\\s*|\\s*Villa Mills\\s*", "PNT", ind.data$Site)
 ind.data$Habitat[ind.data$Site == "Savegre Valley"|ind.data$Site == "Chirripo"|ind.data$Site == "Bosque de Agua"|ind.data$Site == "PNT"|ind.data$Site == "Pejibaye"] <- "Cloud Forest"
