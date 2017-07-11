a2c56779512e9853517eba634267d86986d6c2fb
tjwieg <tjwieg@users.noreply.github.com>
2017-07-11 11:44:09 -0700

    Finished rao spacing test functions +Calculate p-value using table of critical values, based on package "circular" +Added rdata file with critical values, from package "circular" -Gave up on calculating exact p-value, calculate maximum instead (i.e: p < x)

--- R scripts/function.rao.R
+++ R scripts/function.rao.R
@@ -1,58 +1,72 @@
 ##Custom Rao spacing test functions
 
-rao <- function(x) { #Generates the U statistic. x must be a vector of values in radians
-  if (max(x) > 2*pi | min(x) < 0) return("Input must be in radians")
+rao.space.U <- function(x) { #Generates the U statistic in degrees. Input x must be a vector of values in radians
+  p <- 2*pi
+  x <- x[!is.na(x)]
+  if (max(x) > p | min(x) < 0) {
+    warning("Input must be in radians")
+    return(NA)
+  }
+  
   n <- length(x)
-  lambda <- 2*pi/n
+  if (n <= 4) {
+    warning("Sample size too small!")
+    return(NA)
+  }
+  
+  lambda <- 360/n
+  x <- 360*x/p #Converting x to degrees
   
   fi <- x[order(x)]
   fi1 <- c(fi[-1], 0)
   Ti <- fi1 - fi #Calculates the distance between each point after being sorted
-  Ti[n] <- ((2*pi)-fi[n])+fi[1] #Calculates the distance between first and last point, to account for "wrap" at edge of circular distribution
+  Ti[n] <- (p-fi[n])+fi[1] #Calculates the distance between first and last point, to account for "wrap" at edge of circular distribution
   
   U <- 0.5*sum(abs(Ti-lambda))
   return(U)
 }
 
-dU <- function(n) { #Supposed to calculate the U distribution for a given sample size
-  p <- 2*pi
-  
-  combination <- function(a, b) {
-   return(factorial(a)/(factorial(b)*factorial(a-b)))
-  }
-  
-  g <- function(x, j) {
-    return(h(x, j)/(p*factorial(j-1)))
-  }
-  
-  h <- function(x, j) {
-    a <- x/p
-    k <- 0
-    while (k < a) {
-      if (k + 1 > a) {
-        kmax <- k
-        break
-      }
-      else k <- k+1
-    }
-    b <- rep.int(0, kmax+1)
-    for (k in 0:kmax) {
-      b[k] <- ((-1)^k)*combination(j, k)*((a-k)^(j-1))
-    }
-    return(sum(b))
-  }
-  
-  f <- function(u) {
-    s <- rep.int(0, n-1)
-    for (j in 1:(n-1)) {
-      s[j] <- combination(n, j)*((u/p)^(n-j-1))*g(n*u, j)/(factorial(n-j-1)*(n^(j-1)))
-    }
-    return(factorial(n-1)*sum(s))
-  }
-  
-  return(f)
+rao.space.test <- function(x) { #Returns the maximum p-value of the U statistic generated from the input data set x
+  U <- rao.space.U(x)
+  n <- length(x)
+  return(rao.space.p(U, n)) #See below for function rao.space.p()
 }
 
-qU <- function(U, n) { #Supposed to compute the p-value of a given U statistic for the given sample size n
-  integrate(dU(n), 0, U)
+
+#The following is based on work from package "circular", by Ulric Lund and Claudio Agostinelli, licensed under the Gnu GPL v2+
+#Changes primarily include output as return() [rather than cat()] and some readability improvements
+
+rao.space.p <- function(U, n) { #Returns the maximum p-value of obtaining the given U statistic from a sample of size n
+  if (is.na(U)) return(NA)
+  #Load table of critical values and select row based on sample size
+  load("rao.table.rda")
+  ifelse(n <= 30, table.row <- n-3,
+        ifelse(n <= 32, table.row <- 27,
+        ifelse(n <= 37, table.row <- 28,
+        ifelse(n <= 42, table.row <- 29,
+        ifelse(n <= 47, table.row <- 30,
+        ifelse(n <= 62, table.row <- 31,
+        ifelse(n <= 87, table.row <- 32,
+        ifelse(n <= 125, table.row <- 33,
+        ifelse(n <= 175, table.row <- 34,
+        ifelse(n <= 250, table.row <- 35,
+        ifelse(n <= 350, table.row <- 36,
+        ifelse(n <= 450, table.row <- 37,
+        ifelse(n <= 550, table.row <- 38,
+        ifelse(n <= 650, table.row <- 39,
+        ifelse(n <= 750, table.row <- 40,
+        ifelse(n <= 850, table.row <- 41,
+        ifelse(n <= 950, table.row <- 42,
+        table.row <- 43)))))))))))))))))
+  
+  #Returns *maximum* p-value based on sample size and given value of U
+  if (U > rao.table[table.row, 1])
+    return(0.001)
+  else if (U > rao.table[table.row, 2])
+    return(0.01)
+  else if (U > rao.table[table.row, 3])
+    return(0.05)
+  else if (U > rao.table[table.row, 4])
+    return(0.10)
+  else return(1)
 }
