b6104256b2680830e304206c2edc99227d446d37
tjwieg <tjwieg@users.noreply.github.com>
2018-05-25 08:45:44 -0700

    Finished (Hopefully!) function.W.R +Added many comments +w.prob now outputs W statistic and p-value as vector of length 2 +Tested accuracy of chi-square approximation vs randomization

--- R scripts/function.W.R
+++ R scripts/function.W.R
@@ -43,43 +43,45 @@ w.stat <- function(...) {
 }
 
 w.prob <- function(..., trials = 10000, randomize = FALSE) {
-  #Function to calculation p-value of the W statistic of given 2+ vectors of data (in RADIANS)
+  #Function to calculate p-value of the W statistic of given 2+ vectors of data (in RADIANS)
   
   #Data input and organization
   dataList <- list(...)
-  W0 <- do.call(what = w.stat, args = dataList)
+  W0 <- do.call(what = w.stat, args = dataList) #W statistic of actual data
   n <- unlist(lapply(dataList, length)) #n is vector of sample sizes of each input vector
   N <- sum(n) #N is the *total* sample size, how many points were recorded overall
   r <- length(n) #r is the number of vectors input
   
   if(randomize | min(n) < 10) {
     #Randomization test
-    randW <- rep(x = NA, times = trials)
+    randW <- rep(x = NA, times = trials) #Preallocate result vector
     
     for (i in 1:trials) {
       randList <- list()
-      randDat <- sample(x = unlist(dataList), size = N, replace = FALSE)
+      randDat <- sample(x = unlist(dataList), size = N, replace = FALSE) #Randomly resample points for each data vector from total population without replacement
       for (j in 1:r) {
-        if (j == 1) randList[[1]] <- randDat[1:n[j]]
+        if (j == 1) randList[[1]] <- randDat[1:n[j]] #The first "new" data vector has n[1] points
         else {
           nStart <- length(unlist(randList))
-          randList[[j]] <- randDat[(nStart+1):(nStart+n[j])]
+          randList[[j]] <- randDat[(nStart+1):(nStart+n[j])] #The j'th "new" data vector has n[j] points
         }
       }
-      randW[i] <- do.call(what = w.stat, args = randList)
+      randW[i] <- do.call(what = w.stat, args = randList) #Once all "new" data vectors constructed, calculate W of "new" dataset and store it in the randW vector
     }
-    sortW <- sort(randW)
-    p <- which.min(abs(sortW-W0))
-    return(1-(p/trials))
+    sortW <- sort(randW) #Arrange W statistics from randomization in ascending order
+    p <- which.min(abs(sortW-W0)) #Figure out where W0 fits into the randomized distribution
+    return(c("W" = W0, "p-value" = 1-(p/trials))) #p-value = the fraction of trials that gave a W greater than W0
   }
   else {
-    return(pchisq(q = W0, df = 2*r - 2, lower.tail = FALSE))
+    return(c("W" = W0, "p-value" = pchisq(q = W0, df = 2*r - 2, lower.tail = FALSE))) #Approximate with Chi-square
   }
 }
 
-Spring <- c(0,  20,  40,  60, 160, 170,  200, 220, 270, 290, 340, 350)
-Summer <- c(10, 10,  20,  20,  30,  30,   40, 150, 150, 150, 170, 190, 290)
-Autumn <- c(30, 70,  110, 170, 180, 190, 240, 250, 260, 260, 290, 350)
-Winter <- c(50, 120, 190, 210, 220, 250, 260, 290, 290, 320, 320, 340)
+#Sample Dataset, found in Appendix B14 of Fisher 1993.
+# Spring <- c(0,  20,  40,  60, 160, 170,  200, 220, 270, 290, 340, 350)
+# Autumn <- c(30, 70,  110, 170, 180, 190, 240, 250, 260, 260, 290, 350)
+# Winter <- c(50, 120, 190, 210, 220, 250, 260, 290, 290, 320, 320, 340)
 
-print(w.prob(Winter, Spring, Autumn, randomize = TRUE))
+#w.prob(Winter, Spring, Autumn, randomize = TRUE, trials = 1000000) -> 0.452103
+#w.prob(Winter, Spring, Autumn) -> 0.4280753
+#Chisquare has ~5% error in p-value at around p=0.45
